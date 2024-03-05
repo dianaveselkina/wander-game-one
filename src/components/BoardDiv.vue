430aadc04a4d916e7db790d81c14673174745e9e
dianaveselkina <vess_di@mail.ru>
2024-03-05 21:09:38 +0300

    create project

---
+++ src/components/BoardDiv.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="board">
+    <h1>Игра бродилка</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+h1 {
+  font-size: 4rem;
+  font-style: italic;
+  color: #5c564f;
+}
+.board {
+  max-width: 2400px;
+  height: 1800px;
+  background-color: #d1c6bb;
+  position: relative;
+  margin: 2rem auto;
+  border-radius: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
